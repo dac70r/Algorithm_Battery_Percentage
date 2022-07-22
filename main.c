2dc18745d328a4b9256671c348492651d25f6177
Dennis Wong <denniswong178@gmail.com>
2022-07-22 10:33:20 +0800

    Reduced the output Voltage to 2 decimal places

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(){
     
     // test case for program 
     for (float x=42.0;x>=30;x=x-0.1){
-        printf("%f\n",x);
+        printf("%0.2f\n",x);
         check_battery_status(x);
         
     }
