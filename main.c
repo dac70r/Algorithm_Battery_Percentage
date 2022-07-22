2ce1b9c4213893ebb85426f9b9c31981175c14fe
Dennis Wong <denniswong178@gmail.com>
2022-07-22 10:09:28 +0800

    Added test case program for  main.c

--- main.c
+++ main.c
@@ -2,20 +2,12 @@
 #include "battery.h"
 
 int main(){
-
-    check_battery_status(42);
-    check_battery_status(41.5);
-    check_battery_status(41);
-    check_battery_status(40);
-    check_battery_status(39);
-    check_battery_status(38);
-    check_battery_status(37);
-    check_battery_status(36);
-    check_battery_status(35);
-    check_battery_status(34);
-    check_battery_status(33);
-    check_battery_status(32);
-    check_battery_status(31);
-    check_battery_status(30);
+    
+    // test case for program 
+    for (float x=42.0;x>=30;x=x-0.1){
+        printf("%f\n",x);
+        check_battery_status(x);
+        
+    }
 
 }
